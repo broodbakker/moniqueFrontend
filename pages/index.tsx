a993aea7ea180627dfac68cd5d028f41fa92f04f
martijn van de ree <mrtjnvanderee@gmail.com>
2021-03-02 10:40:41 +0100

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+import Head from '../components/head'
+import Nav from '../components/nav'
+
+const Home = () => (
+  <div>test type
+   </div>
+   )
+
+
+
+export default Home
